7122f925a4c519f43125432170aa43c679752b74
Roberth Strand <me@robstr.dev>
2024-10-04 13:08:51 +0200

    feat(tools): added bruno cli
    Install NPM and add the bruno CLI tooling.
    
    Signed-off-by: Roberth Strand <me@robstr.dev>

--- tools.sh
+++ tools.sh
@@ -10,8 +10,18 @@ curl https://releases.hashicorp.com/terraform/1.9.7/terraform_1.9.7_linux_amd64.
 echo "Terraform installed successfully."
 
 # 02. Azure CLI
+echo "Installing Azure CLI..."
 apk add --no-cache py3-pip python3-dev gcc musl-dev linux-headers
 pip install --upgrade pip && pip install azure-cli
+echo "Azure CLI installed successfully."
 
-# 03. Kubernetes tooling
+# 03. Various k8s tools
+echo "Installing k8s tools..."
 apk add kubectl helm
+echo "k8s tools installed successfully."
+
+# 04. Bruno
+echo "Installing Bruno..."
+apk add npm && npm install -g @usebruno/cli@1.32.0
+echo "Bruno installed successfully."
+
